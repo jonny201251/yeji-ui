7342d829135cb526eeb9a1fb19b44d7f56943af2
jonny201251 <jonny201251@163.com>
2021-12-04 21:26:50 +0800

    a

--- src/pages/LeadDept/List.jsx
+++ src/pages/LeadDept/List.jsx
@@ -3,7 +3,7 @@ import { leadDeptPath } from '../../utils'
 
 export default () => {
   let columns = [
-    { title: '公司领导姓名', dataIndex: 'userName', valueType: 'text' }
+    { title: '公司领导姓名', dataIndex: 'userName', valueType: 'text' },
   ]
 
   return <BaseProTable path={leadDeptPath} columns={columns} rowKey={'userName'}/>
