23b2c85d9488e5a57903ccdcc1b6feebe6ef7445
jonny201251 <jonny201251@163.com>
2021-12-22 20:31:34 +0800

    a

--- src/pages/Score/Form.jsx
+++ src/pages/Score/Form.jsx
@@ -1,30 +1,34 @@
 import {
   ArrayItems,
   ArrayTable,
+  Checkbox,
   Form,
   FormItem,
   FormLayout,
   Input,
   NumberPicker,
   PreviewText,
-} from '@formily/antd';
-import { createSchemaField } from '@formily/react';
-import React, { useEffect, useState } from 'react';
-import {
-  Button,
-  Checkbox as Checkboxx,
-  ConfigProvider,
-  Select as Selectt,
   Space,
-} from 'antd';
+  Submit,
+} from '@formily/antd';
+import { createSchemaField, Field } from '@formily/react';
+import React, { useEffect, useState } from 'react';
+import { Button, ConfigProvider } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 import { get, scorePath } from '../../utils';
+import { createForm } from '@formily/core';
+
+const scoreForm = createForm();
 
 export default (props) => {
   let { form, record } = props;
   const [data, setData] = useState([]);
   const [options, setOptions] = useState();
   const [flag, setFlag] = useState(false);
+  const [checkedList, setCheckedList] = useState();
+  const [inputScore, setInputScore] = useState();
+
+  form.setValues({ scoreList: record });
 
   const SchemaField = createSchemaField({
     components: {
@@ -35,28 +39,94 @@ export default (props) => {
       ArrayTable,
       NumberPicker,
       PreviewText,
+      Space,
+      Submit,
+      Checkbox,
     },
   });
 
   const title = () => {
     return (
-      <Space>
-        <Checkboxx.Group
-          options={[
-            { label: '政治素质', value: '政治素质' },
-            { label: '职业素养', value: '职业素养' },
-            { label: '廉洁从业', value: '廉洁从业' },
-          ]}
-          onChange={(values) => console.log(values)}
-        />
-        <Input />
-        <Button>评分</Button>
-      </Space>
+      <Form form={scoreForm}>
+        <Space align={'start'}>
+          <Field
+            required
+            name="arr"
+            decorator={[FormItem]}
+            component={[Checkbox.Group]}
+            dataSource={[
+              { label: '政治素质', value: 'score1' },
+              { label: '职业素养', value: 'score2' },
+              { label: '廉洁从业', value: 'score3' },
+              { label: '决策能力', value: 'score4' },
+              { label: '执行能力', value: 'score5' },
+              { label: '创新能力', value: 'score6' },
+              { label: '工作业绩', value: 'score7' },
+            ]}
+          />
+          <Field
+            name="score"
+            validator={[{ required: true, minimum: 50, maximum: 100 }]}
+            decorator={[FormItem]}
+            component={[
+              NumberPicker,
+              { style: { width: 120 }, placeholder: '分值' },
+            ]}
+          />
+          <Button
+            onClick={async () => {
+              const values = await scoreForm.submit();
+              if (values) {
+                //批量评分
+                let { arr, score } = values;
+                arr.forEach((name) => {
+                  record.forEach((item) => {
+                    item[name] = score;
+                  });
+                });
+                //计算得分
+                record.forEach((item) => {
+                  let totalScore = 0;
+                  for (let i = 1; i <= 7; i++) {
+                    console.log(item.checkkObject);
+                    if (item['score' + i]) {
+                      totalScore += item['score' + i];
+                    }
+                  }
+                  item.totalScore = totalScore;
+                });
+                form.setValues({ scoreList: record });
+              }
+            }}
+          >
+            批量评分
+          </Button>
+          <Button
+            onClick={() => {
+              record.forEach((item) => {
+                let totalScore = 0;
+                for (let i = 1; i <= 7; i++) {
+                  if (item['score' + i]) {
+                    totalScore += item['score' + i];
+                  }
+                }
+                item.totalScore = totalScore;
+              });
+              form.setValues({ scoreList: record });
+            }}
+          >
+            计算得分
+          </Button>
+        </Space>
+      </Form>
     );
   };
 
   useEffect(async () => {
-    form.setInitialValues({ scoreList: record });
+    const dataa = await get(scorePath.getScoreList);
+    if (dataa) {
+      setData(dataa);
+    }
     setOptions([
       { label: '政治素质', value: '政治素质' },
       { label: '职业素养', value: '职业素养' },
@@ -74,10 +144,9 @@ export default (props) => {
             x-component="ArrayTable"
             x-component-props={{
               size: 'small',
-              pagination: { pageSize: 100 },
+              pagination: { pageSize: 200 },
               sticky: true,
               title: title,
-              border: false,
             }}
           >
             <SchemaField.Object>
@@ -133,6 +202,8 @@ export default (props) => {
                   x-decorator="FormItem"
                   name="score1"
                   required
+                  minimum={50}
+                  maximum={100}
                   x-component="NumberPicker"
                 />
               </SchemaField.Void>
@@ -144,6 +215,8 @@ export default (props) => {
                   x-decorator="FormItem"
                   name="score2"
                   required
+                  minimum={50}
+                  maximum={100}
                   x-component="NumberPicker"
                 />
               </SchemaField.Void>
@@ -155,39 +228,8 @@ export default (props) => {
                   x-decorator="FormItem"
                   name="score3"
                   required
-                  x-component="NumberPicker"
-                />
-              </SchemaField.Void>
-              <SchemaField.Void
-                x-component="ArrayTable.Column"
-                x-component-props={{ title: '执行能力' }}
-              >
-                <SchemaField.Number
-                  x-decorator="FormItem"
-                  name="score4"
-                  required
-                  x-component="NumberPicker"
-                />
-              </SchemaField.Void>
-              <SchemaField.Void
-                x-component="ArrayTable.Column"
-                x-component-props={{ title: '创新能力' }}
-              >
-                <SchemaField.Number
-                  x-decorator="FormItem"
-                  name="score5"
-                  required
-                  x-component="NumberPicker"
-                />
-              </SchemaField.Void>
-              <SchemaField.Void
-                x-component="ArrayTable.Column"
-                x-component-props={{ title: '工作业绩' }}
-              >
-                <SchemaField.Number
-                  x-decorator="FormItem"
-                  name="score6"
-                  required
+                  minimum={50}
+                  maximum={100}
                   x-component="NumberPicker"
                 />
               </SchemaField.Void>
@@ -197,14 +239,55 @@ export default (props) => {
               >
                 <SchemaField.Number
                   x-decorator="FormItem"
-                  name="score7"
+                  name="score4"
                   required
+                  minimum={50}
+                  maximum={100}
                   x-component="NumberPicker"
                 />
               </SchemaField.Void>
               <SchemaField.Void
                 x-component="ArrayTable.Column"
-                x-component-props={{ title: '总得分' }}
+                x-component-props={{ title: '执行能力' }}
+              >
+                <SchemaField.Number
+                  x-decorator="FormItem"
+                  name="score5"
+                  required
+                  minimum={50}
+                  maximum={100}
+                  x-component="NumberPicker"
+                />
+              </SchemaField.Void>
+              <SchemaField.Void
+                x-component="ArrayTable.Column"
+                x-component-props={{ title: '创新能力' }}
+              >
+                <SchemaField.Number
+                  x-decorator="FormItem"
+                  name="score6"
+                  required
+                  minimum={50}
+                  maximum={100}
+                  x-component="NumberPicker"
+                />
+              </SchemaField.Void>
+              <SchemaField.Void
+                x-component="ArrayTable.Column"
+                x-component-props={{ title: '工作业绩' }}
+              >
+                <SchemaField.Number
+                  x-decorator="FormItem"
+                  name="score7"
+                  required
+                  minimum={50}
+                  maximum={100}
+                  x-component="NumberPicker"
+                />
+              </SchemaField.Void>
+              <SchemaField.Void
+                x-component="ArrayTable.Column"
+                x-component-props={{ title: '得分' }}
               >
                 <SchemaField.Number
                   x-decorator="FormItem"
